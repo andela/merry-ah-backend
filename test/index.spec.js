7da6447cc48415eb5cca9882628340655062f193
Didun Morodolu <elkenzom1@gmail.com>
2019-01-08 13:28:31 +0100

    chore(tests): Setup unit testing
    - To ensure the back end has all functionality working as intended
    
    [Finishes #162727458]

---
+++ test/index.spec.js
@@ -0,0 +1,18 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+// import app from '../index.js';
+
+chai.use(chaiHttp);
+chai.should();
+describe("Google", () => {
+  describe("GET /", () => {
+    it("should reach google homepage", (done) => {
+      chai.request('https://google.com.ng')
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+});
