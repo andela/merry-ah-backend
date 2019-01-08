92ace894440347fd90b4a7787cc591258ef8a20a
Proception <omasan.esimaje@gmail.com>
2019-01-08 13:59:08 +0100

    chore(database): fix eslint errors
    - fix eslint errors

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import Sequelize  from 'sequelize';
+import Sequelize from 'sequelize';
 import config from '../config/config';
 
 const basename = path.basename(__filename);
@@ -19,20 +19,26 @@ let sequelize;
 if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, genericOptions);
 } else {
-  sequelize = new Sequelize(envConfig.database, envConfig.username, envConfig.password, genericOptions);
+  sequelize = new Sequelize(
+    envConfig.database,
+    envConfig.username,
+    envConfig.password,
+    genericOptions
+  );
 }
 
 fs
   .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
+  .filter(
+    file => (file
+      .indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
+  )
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
