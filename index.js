80219c044449fb5a20e24a4710fdcea73811c3dc
Proception <omasan.esimaje@gmail.com>
2019-01-09 11:58:18 +0100

    chore(deploy): setup base application
        - add response object
        - install express-request-id to add a unique id to all requests
        - setup server environment
        - add catch all route for malformed urls
        - add index route for home
        - specified api versioning server
    
    [Finishes #162727457]

--- index.js
+++ index.js
@@ -1,93 +1,63 @@
-const fs = require("fs"),
-    http = require("http"),
-    path = require("path"),
-    methods = require("methods"),
-    express = require("express"),
-    bodyParser = require("body-parser"),
-    session = require("express-session"),
-    cors = require("cors"),
-    passport = require("passport"),
-    errorhandler = require("errorhandler"),
-    mongoose = require("mongoose");
-
-const isProduction = process.env.NODE_ENV === "production";
+import express from 'express';
+import bodyParser from 'body-parser';
+// import errorhandler from 'errorhandler';
+import cors from 'cors';
+import morgan from 'morgan';
+import requestId from 'express-request-id';
+import routes from './src/routes/index';
+import Response from './src/helpers/response';
+// import db from './src/db/models/index';
 
 // Create global app object
 const app = express();
 
+const port = process.env.PORT || 9000;
+
+let response;
+
 app.use(cors());
 
-// Normal express config defaults
-app.use(require("morgan")("dev"));
+app.use(requestId());
+
+morgan.token('id', req => req.id);
+
+const loggerFormat = ':id [:date[web]] ":method :url" :status :response-time';
+
+app.use(morgan(loggerFormat, {
+  skip: (req, res) => res.statusCode < 400, stream: process.stderr
+}));
+
+app.use(morgan(loggerFormat, {
+  skip: (req, res) => res.statusCode >= 400, stream: process.stdout
+}));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(require("method-override")());
-app.use(express.static(__dirname + "/public"));
+app.use('/api/v1/', routes);
 
-app.use(
-    session({
-        secret: "authorshaven",
-        cookie: { maxAge: 60000 },
-        resave: false,
-        saveUninitialized: false
-    })
-);
+app.all('/', ((req, res) => {
+  response = new Response(
+    'ok',
+    200,
+    'Welcome to Authors Haven',
+    {}
+  );
+  return res.status(response.code).json(response);
+}));
 
-if (!isProduction) {
-    app.use(errorhandler());
-}
+app.all('/*', ((req, res) => {
+  response = new Response(
+    'ok',
+    404,
+    `Specified route does not exist ${req.originalUrl}`,
+    {}
+  );
+  return res.status(response.code).json(response);
+}));
 
-if (isProduction) {
-    mongoose.connect(process.env.MONGODB_URI);
-} else {
-    mongoose.connect("mongodb://localhost/conduit");
-    mongoose.set("debug", true);
-}
-
-require("./models/User");
-
-app.use(require("./routes"));
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    const err = new Error("Not Found");
-    err.status = 404;
-    next(err);
+app.listen(port, () => {
+  console.log(`Authors Haven App is listening on port ${port}! `);
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (!isProduction) {
-    app.use(function(err, req, res, next) {
-        console.log(err.stack);
-
-        res.status(err.status || 500);
-
-        res.json({
-            errors: {
-                message: err.message,
-                error: err
-            }
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-        errors: {
-            message: err.message,
-            error: {}
-        }
-    });
-});
-
-// finally, let's start our server...
-const server = app.listen(process.env.PORT || 3000, function() {
-    console.log("Listening on port " + server.address().port);
-});
+export default app;
