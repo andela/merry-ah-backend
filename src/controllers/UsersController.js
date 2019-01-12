95a39e27dab56251567afa1d1646d7528ac401cf
abejide001 <abejidefemi1@gmail.com>
2019-01-12 11:11:09 +0100

    feat(login): implement login functionality fixed jsdoc comments errors implement error handling for bad login details fix eslint errors add seeder file modify test script in package.json add script to check linting errors fix houndci errors move compare password logic into the helper file [Delivers #163052933]

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,90 +1,157 @@
+/* eslint-disable valid-jsdoc */
 import models from '../db/models';
 import TokenAuthenticate from '../helpers/TokenAuthenticate';
 import Response from '../helpers/response';
-import passwordHash from '../helpers/passwordHash';
-import EmailNotificationAPI from '../helpers/EmailNotificationAPI.js'; 
+import { passwordHash, comparePassword } from '../helpers/passwordHash';
+import EmailNotificationAPI from '../helpers/EmailNotificationAPI';
+
 const { User, Profile } = models;
 
-
-/**
- * @description Defines the actions to for the users endpoints
- * @class UserController
- */
 
 let response;
 const tokenExpireTime = '10hr';
 const salt = 10;
-
-
- class UsersController {
-  
-   static async signUp(req, res){
+/** Users Controller Class */
+class UsersController {
+  /**
+   * @static
+   * @desc POST /api/v1/auth/signup
+   * @param {object} req
+   * @param {object} res
+   * @memberof UsersController
+   * @returns user details
+   */
+  static async signUp(req, res) {
     const defaultstatus = 0;
 
-      const signUpType = 'local';
-    try{
-    const { firstName, lastName, email, username, password, userType } = req.body;
-    let { bio } = req.body || null;
-    let { imgURL } = req.body || null;
-    const hash = await passwordHash(password, salt);
+    const signUpType = 'local';
+    try {
+      const {
+        firstName, lastName, email, username, password, userType
+      } = req.body;
+      const { bio } = req.body || null;
+      const { imgURL } = req.body || null;
+      const hash = await passwordHash(password, salt);
 
       const signup = await User
-      .create({
-        email,
-        username,
-        password: hash,
-        userType,
-        signUpType,
-        isVerified: defaultstatus
-      });
-      const { id, username: registeredUsername, email: registeredEmail, userType: userSignupType }  = signup.dataValues;
+        .create({
+          email,
+          username,
+          password: hash,
+          userType,
+          signUpType,
+          isVerified: defaultstatus
+        });
+      const {
+        id, username: registeredUsername, email: registeredEmail,
+        userType: userSignupType
+      } = signup.dataValues;
       await Profile
-      .create({
-        userId: id,
-        firstName,
-        lastName,
-        bio,
-        imgURL,
-      });
-      const userDetails = {id, registeredUsername, registeredEmail, userSignupType };
-      const token = await TokenAuthenticate.generateToken(userDetails, tokenExpireTime);
+        .create({
+          userId: id,
+          firstName,
+          lastName,
+          bio,
+          imgURL,
+        });
+      const userDetails = {
+        id, registeredUsername, registeredEmail, userSignupType
+      };
+      const token = await TokenAuthenticate
+        .generateToken(userDetails, tokenExpireTime);
       const recipient = registeredEmail;
-      const subject = "Email Verification";
+      const subject = 'Email Verification';
       const message = `<h1>Verification link</h1><br>
-        <a href='http://localhost:9000/api/v1/user?token=${token}'><button style='font-size: 20px; background: orange;'>verify</button></a><br>
-        <p>Kindly click on the button above to verify your email. This link will <strong>expire in 10hrs</strong></p>
+        <a href='http://localhost:9000/api/v1/user?token=${token}'>
+        <button style='font-size: 20px; background: orange;'>verify</button>
+        </a><br>
+        <p>Kindly click on the button above to verify your email. 
+        This link will <strong>expire in 10hrs
+        </strong></p>
         `;
-        const sendVerificationLink = new EmailNotificationAPI({
+      const sendVerificationLink = new EmailNotificationAPI({
         recipient,
         subject,
         message,
-    });
-  const sendMail = await sendVerificationLink.sendEmail();
- if(sendMail !== 'Message sent'){
-    response = new Response(
-    'Bad request',
-    400,
-    `There was a problem sending`,
-  );
-  return res.status(response.code).json(response);
- }
-  response = new Response(
-    'Ok',
-    201,
-    `User created successfully and verification link sent to your Email`,
-    {token}
-  );
+      });
+      const sendMail = await sendVerificationLink.sendEmail();
+      if (sendMail !== 'Message sent') {
+        response = new Response(
+          'Bad request',
+          400,
+          'There was a problem sending',
+        );
+        return res.status(response.code).json(response);
+      }
+      response = new Response(
+        'Ok',
+        201,
+        'User created successfully and verification link sent to your Email',
+        { token }
+      );
+      return res.status(response.code).json(response);
+    } catch (err) {
+      response = new Response(
+        'Not ok',
+        500,
+        `${err}`,
+      );
       return res.status(response.code).json(response);
     }
-    catch(err){
-        response = new Response(
-            'Not ok',
-            500,
-            `${err}`,
-          );
-          return res.status(response.code).json(response);
-    }
-   }
- }
+  }
 
- export default UsersController;
+  /**
+   * @static
+   * @desc POST /api/v1/auth/signin
+   * @param {object} req
+   * @param {object} res
+   * @memberof UsersController
+   * @returns user token
+   */
+  static async signIn(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ where: { email } });
+      if (!user) {
+        response = new Response(
+          'Bad Request',
+          400,
+          'Invalid Credentials'
+        );
+        return res.status(response.code).json(response);
+      }
+      if (!comparePassword(password, user.password)) {
+        response = new Response(
+          'Bad Request',
+          400,
+          'Invalid Credentials'
+        );
+        return res.status(response.code).json(response);
+      }
+      const {
+        id, username, email: userEmail, signUpType
+      } = user;
+      const userDetails = {
+        id, username, userEmail, signUpType
+      };
+      const token = await TokenAuthenticate
+        .generateToken(userDetails, tokenExpireTime);
+      response = new Response(
+        'Ok',
+        200,
+        'User logged in successfully',
+        { token }
+      );
+      return res.status(response.code).json(response);
+    } catch (err) {
+      response = new Response(
+        'Not ok',
+        500,
+        `${err}`,
+      );
+      return res.status(response.code).json(response);
+    }
+  }
+}
+
+export default UsersController;
--- test/controllers/userControllerTest.js
+++ test/controllers/userControllerTest.js
@@ -1,4 +1,4 @@
-import 'babel-polyfill'
+import 'babel-polyfill';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../../src/index';
@@ -8,12 +8,11 @@ import { userDetails } from '../mocks/testData';
 chai.use(chaiHttp);
 const { expect } = chai;
 const {
-    validUser,
-    invalidUser
+  validUser,
+  invalidUser
 } = userDetails;
-let token;
-describe('Users Endpoint API Test', () => {
 
+describe('Users Endpoint API Test', () => {
   describe('USERS POST REQUESTS', () => {
     it('it should signup a valid user', (done) => {
       chai.request(app)
@@ -21,32 +20,73 @@ describe('Users Endpoint API Test', () => {
         .send(validUser)
         .end((err, res) => {
           expect(res.body).to.be.a('object');
-          expect(res.body.messages).eql('User created successfully and verification link sent to your Email');
+          expect(res.body.messages)
+            .eql('User created successfully and verification link sent to your Email');
           expect(res.body.data).to.have.property('token');
           expect(res.status).to.equal(201);
-          token = res.body.data.token;
-          done();
+          done(err);
         });
     });
     it('it should not signup an invalid user', (done) => {
+      chai.request(app)
+        .post('/api/v1/auth/signup')
+        .send({})
+        .end((err, res) => {
+          expect(res.body.status).eql('Not ok');
+          done(err);
+        });
+    });
+    it('it should not signup an invalid email', (done) => {
+      chai.request(app)
+        .post('/api/v1/auth/signup')
+        .send(invalidUser)
+        .end((err, res) => {
+          expect(res.body.status).eql('Bad request');
+          expect(res.body.messages).eql('There was a problem sending');
+          done(err);
+        });
+    });
+    it('it should not sign in a user with an email that does not exist',
+      (done) => {
         chai.request(app)
-          .post('/api/v1/auth/signup')
-          .send({})
+          .post('/api/v1/auth/signin')
+          .send(invalidUser.email, invalidUser.password)
           .end((err, res) => {
-            expect(res.body.status).eql('Not ok');
-            done();
+            expect(res.body.status).eql('Bad Request');
+            expect(res.body.messages).eql('Invalid Credentials');
+            done(err);
           });
       });
-      it('it should not signup an invalid email', (done) => {
+    it('it should not sign in a user with a password that does not exist',
+      (done) => {
+        const user = {
+          email: 'julietezekwe@gmail.com',
+          password: 'pocococo'
+        };
         chai.request(app)
-          .post('/api/v1/auth/signup')
-          .send(invalidUser)
+          .post('/api/v1/auth/signin')
+          .send(user)
           .end((err, res) => {
-            expect(res.body.status).eql('Bad request');
-            expect(res.body.messages).eql('There was a problem sending');
-            done();
+            expect(res.body.status).eql('Bad Request');
+            expect(res.body.messages).eql('Invalid Credentials');
+            done(err);
           });
       });
+    it('it should sign in a user with valid email and password', (done) => {
+      chai.request(app)
+        .post('/api/v1/auth/signin')
+        .send({
+          email: 'julietezekwe@gmail.com',
+          password: 'femi'
+        })
+        .end((err, res) => {
+          expect(res.body.messages).eql('User logged in successfully');
+          expect(res.body.data.token).to.be.a('string');
+          expect(res.body.data).to.have.property('token');
+          expect(res.status).to.equal(200);
+          expect(res.body.status).eql('Ok');
+          done(err);
+        });
+    });
   });
- 
 });
