1c26c835f9f8f1b4e1515c08e55ae2a2e06dbac4
daniellamarr <danielchidiebele@gmail.com>
2019-01-15 13:25:40 +0100

    feature(feature/162727466/users-should-be-able-to-reset-password): Modified validation method [Fixes #162727466]

--- src/helpers/Validation.js
+++ src/helpers/Validation.js
@@ -8,7 +8,7 @@ class Validation {
    * @returns {boolean} true|false
    */
   static checkEmptyField(field) {
-    if (field === '' || field === null || field === undefined) {
+    if (!field) {
       return false;
     }
     return true;
