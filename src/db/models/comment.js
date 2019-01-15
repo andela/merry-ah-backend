0b2756340a963e1377f7f388033ef0dcd99455e1
Proception <omasan.esimaje@gmail.com>
2019-01-15 17:43:43 +0100

    chore(database): add models for database in sequelize
    - add art model
    - add category model
    - add comment model
    - add following model
    - add followingsummary model
    - add like model
    - add likesummary model
    - add media model
    - add rate, transaction and updatedcomments model
    
    [Finishes #163017657]

---
+++ src/db/models/comment.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const Comment = sequelize.define('Comment', {
+    artId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
+    body: DataTypes.STRING
+  }, {});
+  Comment.associate = function (models) {
+    // associations can be defined here
+    Comment.belongsTo(models.Art, {
+      foreignKey: 'artId',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    });
+    Comment.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    });
+    Comment.hasMany(models.UpdatedComment, {
+      foreignKey: 'commentId'
+    });
+  };
+  return Comment;
+};
