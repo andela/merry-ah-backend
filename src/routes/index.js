90b52b006ddce46169d1d347059ba28cd4fb81c3
abejide001 <abejidefemi1@gmail.com>
2019-01-16 22:22:00 +0100

    feat(document-api): document api with swagger setup swagger configuration use swagger jsdoc in authrouter [Delivers #162727461]

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import authRouter from './authRouter';
 
 const router = express.Router();
+const swaggerSpec = swaggerJSDoc(require('../utils/swaggerConfig')
+  .options);
 
 router.use('/auth', authRouter);
-
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 export default router;
