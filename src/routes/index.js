38cca94572221a097472bb5e7d076f6a64a330f3
Proception <omasan.esimaje@gmail.com>
2019-01-17 14:00:45 +0100

    feat(rating): add routes and middlewares for rating implementation
    - add rating router to handle rating requests
    - add rating middlewares to handle validations and responses
    
    [Finishes #162727470]

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import authRouter from './authRouter';
+import ratingRouter from './ratingRouter';
+import TokenAuthenticate from '../helpers/TokenAuthenticate';
 
 const router = express.Router();
 
 router.use('/auth', authRouter);
+router.use('/rate', TokenAuthenticate.tokenVerify, ratingRouter);
 
 export default router;
---
+++ src/routes/ratingRouter.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import RatingController from '../controllers/RatingController';
+import RatingValidator from '../middlewares/RatingValidator';
+
+const ratingRouter = express.Router();
+
+ratingRouter.post('/:artId',
+  RatingValidator.genericRatingValidator,
+  RatingValidator.addRatingValidator,
+  RatingController.addItemRating);
+ratingRouter.get('/:artId',
+  RatingValidator.genericRatingValidator,
+  RatingController.getItemRating);
+ratingRouter.get('/:artId/user',
+  RatingValidator.genericRatingValidator,
+  RatingController.getUserItemRating);
+
+export default ratingRouter;
