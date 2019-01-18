1649d6a1fca677de16ce74fc64bb1dc16ab1639c
abejide001 <abejidefemi1@gmail.com>
2019-01-18 12:50:49 +0100

    fix(hound): fix hound errors

--- src/routes/socialRouter.js
+++ src/routes/socialRouter.js
@@ -4,22 +4,34 @@ import passport from '../services/passport';
 
 const socialRouter = express.Router();
 
-socialRouter.get('/facebook',
-  passport.authenticate('facebook', { scope: ['email'] }));
+socialRouter.get(
+  '/facebook',
+  passport.authenticate('facebook', { scope: ['email'] })
+);
 
-socialRouter.get('/facebook/callback',
-  passport.authenticate('facebook'), SocialController.getToken);
-socialRouter.get('/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] }));
+socialRouter.get(
+  '/facebook/callback',
+  passport.authenticate('facebook'), SocialController.getToken
+);
+socialRouter.get(
+  '/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
 
-socialRouter.get('/google/callback',
-  passport.authenticate('google'), SocialController.getToken);
+socialRouter.get(
+  '/google/callback',
+  passport.authenticate('google'), SocialController.getToken
+);
 
-socialRouter.get('/twitter',
-  passport.authenticate('twitter', { scope: ['include_email =true'] }));
+socialRouter.get(
+  '/twitter',
+  passport.authenticate('twitter', { scope: ['include_email =true'] })
+);
 
-socialRouter.get('/twitter/callback',
-  passport.authenticate('twitter'), SocialController.getToken);
+socialRouter.get(
+  '/twitter/callback',
+  passport.authenticate('twitter'), SocialController.getToken
+);
 
 // eslint-disable-next-line import/prefer-default-export
 export default socialRouter;
