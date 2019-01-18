83a562677d1769dee69ed62f34c755cf4b4b357a
Proception <omasan.esimaje@gmail.com>
2019-01-18 11:02:18 +0100

    fix(hound): fix hound errors

--- src/db/migrations/20190117014046-create-rate-summary.js
+++ src/db/migrations/20190117014046-create-rate-summary.js
@@ -1,32 +1,31 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('RateSummaries', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      artId: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'Arts',
-          key: 'id',
-          as: 'artId',
-        }
-      },
-      caculatedRate: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+  up: (queryInterface,
+    Sequelize) => queryInterface.createTable('RateSummaries', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    artId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Arts',
+        key: 'id',
+        as: 'artId',
       }
-    });
-  },
+    },
+    caculatedRate: {
+      type: Sequelize.INTEGER
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
   down: queryInterface => queryInterface.dropTable('RateSummaries')
 };
