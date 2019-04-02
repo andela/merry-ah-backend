ca582e7a177f48ae08f25f662ad8fc14d6ba653d
Didun Morodolu <elkenzom1@gmail.com>
2019-04-02 19:57:50 +0100

    Allow DB_HOST to be configured from .env

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -2,28 +2,28 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const {
-  DB_USER, DB_NAME, DB_PASSWORD, DB_NAME_TEST
+  DB_USER, DB_NAME, DB_PASSWORD, DB_NAME_TEST, DB_HOST,
 } = process.env;
 module.exports = {
   development: {
     username: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME,
-    host: '127.0.0.1',
+    host: DB_HOST,
     dialect: 'postgres'
   },
   test: {
     username: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME_TEST,
-    host: '127.0.0.1',
+    host: DB_HOST,
     dialect: 'postgres'
   },
   production: {
     username: 'root',
     password: null,
     database: 'database_production',
-    host: '127.0.0.1',
+    host: DB_HOST,
     dialect: 'postgres'
   }
 };
