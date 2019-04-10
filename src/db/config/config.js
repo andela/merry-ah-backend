919fb4d8ff0ec46efbf4ae09b22cf089e7d5ea30
Didun Morodolu <elkenzom1@gmail.com>
2019-04-10 17:19:48 +0100

    Configure sequelize config

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const {
-  DB_USER, DB_NAME, DB_PASSWORD, DB_NAME_TEST, DB_HOST,
+  DB_USER, DB_NAME, DB_PASSWORD, DB_NAME_TEST, DB_HOST, DB_PORT = 5432,
 } = process.env;
 module.exports = {
   development: {
@@ -10,20 +10,24 @@ module.exports = {
     password: DB_PASSWORD,
     database: DB_NAME,
     host: DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    port: DB_PORT
   },
   test: {
     username: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME_TEST,
     host: DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    port: DB_PORT
+
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
     host: DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    port: DB_PORT
   }
 };
